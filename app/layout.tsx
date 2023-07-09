8062e9138ae11defdaa32cefc8d75012475743c1
Phahladira-Moja <phahladira@patala.co.za>
2023-07-09 19:37:35 +0200

    Created the button component as well as busy with the Modal component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import ClientOnly from "./components/ClientOnly";
+import Modal from "./components/modals/Modal";
 import Navbar from "./components/navbar/Navbar";
 import "./globals.css";
 import { Nunito } from "next/font/google";
@@ -19,6 +20,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ClientOnly>
+          {/* Nb isOpen == isOpen={true} */}
+          <Modal actionLabel="Submit" title="Login Modal" isOpen />
           <Navbar />
         </ClientOnly>
         {children}
