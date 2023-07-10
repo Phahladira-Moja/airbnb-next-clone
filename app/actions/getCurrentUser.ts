e4093dda219bb78a7a02a26e65302d9088980b48
Phahladira-Moja <phahladira@patala.co.za>
2023-07-11 00:17:54 +0200

    Completed Login flow
    - Created the LoginModal and hook
    - Added the props for both Navbar and UserMenu to take currentUser Object
    - Created an action to get the current user session by retrieving logged in user
    - Added the the getCurrentUser call in the layout class to pass  it to the entire app.

---
+++ app/actions/getCurrentUser.ts
@@ -0,0 +1,33 @@
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import prisma from "@/app/libs/prismadb";
+
+export async function getSession() {
+  return await getServerSession(authOptions);
+}
+
+export default async function getCurrentUser() {
+  // NB this is not api call, this is a direct server connection
+  // using the server component
+  try {
+    const session = await getSession();
+
+    if (!session?.user?.email) {
+      return null;
+    }
+
+    const currentUser = await prisma.user.findUnique({
+      where: {
+        email: session.user.email as string,
+      },
+    });
+
+    if (!currentUser) {
+      return null;
+    }
+
+    return currentUser;
+  } catch (error) {
+    return null;
+  }
+}
