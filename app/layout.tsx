87f45838dfcd8bde51cad6a8c2a0db12ed016095
Phahladira-Moja <phahladira@patala.co.za>
2023-07-11 21:15:29 +0200

    Created the RentModal and useRentModal Hook
    - Added the logic to display the rentmodal
    - Added the logic to use the already have account and need account event

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Nunito } from "next/font/google";
 import ToasterProvider from "./providers/ToasterProvider";
 import LoginModal from "./components/modals/LoginModal";
 import getCurrentUser from "./actions/getCurrentUser";
+import RentModal from "./components/modals/RentModal";
 
 const inter = Nunito({ subsets: ["latin"] });
 
@@ -28,6 +29,7 @@ export default async function RootLayout({
           <ToasterProvider />
           <RegisterModal />
           <LoginModal />
+          <RentModal />
           <Navbar currentUser={currentUser} />
         </ClientOnly>
         {children}
