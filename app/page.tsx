8b92e66a80415e7e0dfc0a71f93fc9030e4b9fba
Phahladira-Moja <phahladira@patala.co.za>
2023-07-13 01:26:29 +0200

    Completed the fetch listing view/component/action
    - Added the HeartButton comp withouot functionality
    - Added the LisitngCard comp without functionality
    - Added nav to specific listing page, but page not created yet
    - created the get listing server action

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,15 @@ import Image from "next/image";
 import ClientOnly from "./components/ClientOnly";
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
+import getListings from "./actions/getListings";
+import ListingCard from "./components/listings/ListingCard";
+import getCurrentUser from "./actions/getCurrentUser";
 
-export default function Home() {
-  const isEmpty = true;
+export default async function Home() {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
 
-  if (isEmpty) {
+  if (listings.length === 0) {
     return (
       <ClientOnly>
         <EmptyState showReset />
@@ -30,7 +34,15 @@ export default function Home() {
             gap-8
           "
         >
-          <div>My future listings</div>
+          {listings.map((listing) => {
+            return (
+              <ListingCard
+                key={listing.id}
+                data={listing}
+                currentUser={currentUser}
+              />
+            );
+          })}
         </div>
       </Container>
     </ClientOnly>
